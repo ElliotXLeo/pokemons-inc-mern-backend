7f53c2fc3b3ac54ba345724c1a212cb55a7d08ce
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-18 11:31:10 -0500

    variables de entorno & config env

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import { PORT } from './config/env.js';
 import pokemonRouter from './routes/pokemon.routes.js';
 
 const app = express();
-app.listen(2050);
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo desde el puerto ${PORT}`);
+});
 
 app.use('/api', pokemonRouter);
