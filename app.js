c11bd1be771b13f8350bfc68c842a381c7a8770a
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-18 13:51:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import pokemonRouter from './routes/pokemon.routes.js';
 
 const app = express();
+app.use(express.json());
+
 app.use('/api', pokemonRouter);
 
 export default app;
