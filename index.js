b91c0bf19a654770dfb8a7eeb31fbbe0b6a6d3a1
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-18 12:54:09 -0500

    config db & Update config env

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import db from './config/db.js';
 import { PORT } from './config/env.js';
 import pokemonRouter from './routes/pokemon.routes.js';
 
 const app = express();
+db();
 app.listen(PORT, () => {
   console.log(`Servidor corriendo desde el puerto ${PORT}`);
 });
