8f17076f143c5dfcefe9b7dc9f922c630ee82cde
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-19 11:50:26 -0500

    Configuración de cloudinary

---
+++ config/cloudinary.js
@@ -0,0 +1,18 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_CLOUD_NAME } from './env.js';
+
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET
+});
+
+export const uploadFile = async (filePath) => {
+  try {
+    return await cloudinary.uploader.upload(filePath, {
+      folder: 'pokemon-inc-mern'
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
