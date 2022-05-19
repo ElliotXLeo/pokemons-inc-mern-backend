5bbb33f34ff7f4743cb2170f9e1f308a8b286ab4
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-19 10:33:37 -0500

    Configuración de express-fileupload

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import fileUpload from 'express-fileupload';
 import pokemonRouter from './routes/pokemon.routes.js';
 
 const app = express();
 app.use(express.json());
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: './upload'
+}));
 
 app.use('/api', pokemonRouter);
 
